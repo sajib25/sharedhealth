45da80d938ab5b1af893534eb83bb2ae4c238e4a
MD Dubey <mdinfotech007@gmail.com>
2016-09-10 11:07:31 +0530

    Dubey, Neha | SHR-1344 | Deleted migration and test usage for unused columnfamily generated_hid_range.

--- healthId-api/src/test/java/org/sharedhealth/healthId/web/utils/TestUtil.java
+++ healthId-api/src/test/java/org/sharedhealth/healthId/web/utils/TestUtil.java
@@ -1,17 +1,10 @@
 package org.sharedhealth.healthId.web.utils;
 
-import com.datastax.driver.core.utils.UUIDs;
 import org.springframework.data.cassandra.core.CassandraOperations;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.addAll;
-import static java.util.Collections.unmodifiableList;
 import static org.sharedhealth.healthId.web.repository.RepositoryConstants.*;
 
 public class TestUtil {
@@ -26,25 +19,10 @@ public class TestUtil {
     private static List<String> getAllColumnFamilies() {
         return asList(
                 CF_MCI_HEALTH_ID,
-                CF_GENERATED_HID_RANGE,
                 CF_GENERATED_HID_BLOCKS,
                 CF_ORG_HEALTH_ID
         );
     }
 
-    public static Set<String> asSet(String... values) {
-        Set<String> set = new HashSet<>();
-        addAll(set, values);
-        return set;
-    }
 
-
-    public static List<UUID> buildTimeUuids() throws InterruptedException {
-        List<UUID> timeUuids = new ArrayList<>();
-        for (int i = 1; i <= 20; i++) {
-            timeUuids.add(UUIDs.timeBased());
-            Thread.sleep(1);
-        }
-        return unmodifiableList(timeUuids);
-    }
 }
